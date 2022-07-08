0f4623580f0810017c1d4bb6c78cac11588b3fd9
Stef Chateaubreaund <103604314+Stef-Chateaubreaund@users.noreply.github.com>
2022-07-08 18:21:30 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+export default App;
+
+
+
+
+
+function App() {
+    return (
+    <div className="App">
+        <h2>Hello Dojo!</h2>
+    
+        <h3>Things i need to do:</h3>
+    <ul>
+        <li>learn react</li>
+        <li>climb mt everest</li>
+        <li>run a marathon</li>
+        <li>feed the fishes</li>
+    </ul>
+    </div>
+);
+}
+
